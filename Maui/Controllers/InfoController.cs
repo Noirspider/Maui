4fc9ebb8c56120a93381160c02ea372af47f63a8
EAGLE-3 <EAGLE-3@EAGLE-1>
2024-04-26 12:16:35 +0200

    backup 26/04

---
+++ Maui/Controllers/InfoController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Maui.Controllers
+{
+    public class InfoController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Contatti()
+        {
+            return View();
+        }
+        public IActionResult ChiSiamo()
+        {
+            return View();
+        }
+    }
+}
